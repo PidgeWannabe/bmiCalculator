ab90788fe6cedf56fb5c289873d939caa263a654
mossTheBoss <43779200+PidgeWannabe@users.noreply.github.com>
2019-05-01 11:06:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bmiCalculator(weight, height) {
-    bmi = weight / Math.pow(height, 2);
+    var bmi = weight / Math.pow(height, 2);
     if(bmi < 18.5){
         alert("Your BMI is " + bmi + " so you are underweight.");
     } else if(bmi >= 18.5 && bmi <= 24.9) {
