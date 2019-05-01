f73fdd00c6a2d1fafb984f0470e933e4b54e372d
mossTheBoss <43779200+PidgeWannabe@users.noreply.github.com>
2019-05-01 10:34:59 +0200

    Finished BMI Calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function bmiCalculator(weight, height) {
+    bmi = weight / Math.pow(height, 2);
+    if(bmi < 18.5){
+        alert("Your BMI is " + bmi + " so you are underweight.");
+    } else if(bmi >= 18.5 && bmi <= 24.9) {
+        alert("Your BMI is " + bmi.toFixed(1) + " so you have a normal weight.");
+    } else if(bmi > 24.9){
+        alert("Your BMI is " + bmi.toFixed(1) + " so you are overweight.");
+    }
+    // toFixed() retroune une string un chiffre après la virgule
+    return bmi.toFixed(1);
+    // return interpretation;
+}
+
+bmiCalculator(prompt("What is your weight(kg, input only numbers separated by a period) ? "), prompt("What is your height(m, input only numbers separated by a period) ? "));
